3c599929fbfad5ae2a7a58e1141338ea87d8d9e2
Daniel Guo <dguo456@gmail.com>
2022-05-14 20:48:33 -0500

    Add files via upload

--- BFS/拓扑排序系列.py
+++ BFS/拓扑排序系列.py
@@ -6,6 +6,8 @@
 # 815 · Course Schedule IV
 # 305 · longest increasing path in a matrix
 # 1469 · Longest Path On The Tree
+# 892 · Alien Dictionary
+# 1876 · Alien Dictionary(easy)
 
 ################################################################################################################
 
@@ -588,3 +590,88 @@ class Solution:
                 distance[neighbor] = distance[node] + edge_length
         
         return max_node, max_distance
+
+
+
+
+
+
+
+# 892 · Alien Dictionary
+"""
+There is a new alien language which uses the latin alphabet. 
+However, the order among letters are unknown to you. You receive a list of non-empty words 
+from the dictionary, where words are sorted lexicographically by the rules of this new language. 
+Derive the order of letters in this language.
+1. You may assume all letters are in lowercase.
+2. The dictionary is invalid, if string a is prefix of string b and b is appear before a.
+3. If the order is invalid, return an empty string.
+4. There may be multiple valid order of letters, return the smallest in normal lexicographical order.
+5. The letters in one string are of the same rank by default and are sorted in Human dictionary order.
+
+Input: ["wrt","wrf","er","ett","rftt"]
+Output: "wertf"
+"""
+from heapq import *
+class Solution:
+    """
+    @param words: a list of words
+    @return: a string which is correct order
+    """
+    def alienOrder(self, words):
+        
+        # Construct Graph
+        Graph = {ch: [] for word in words for ch in word}
+        indegree = {ch: 0 for word in words for ch in word}
+        
+        for pos in range(len(words)-1):
+            for i in range(min(len(words[pos]), len(words[pos+1]))):
+                prev, curr = words[pos][i], words[pos+1][i]
+                if prev != curr:
+                    Graph[prev].append(curr)
+                    indegree[curr] += 1
+                    break
+        if prev == curr and len(words[pos]) > len(words[pos+1]):
+            return ""
+                
+        # Topological sort
+        heap = [ch for ch in indegree if indegree[ch] == 0]
+        heapify(heap)
+        order = []
+        while heap:
+            for _ in range(len(heap)):
+                ch = heappop(heap)
+                order.append(ch)
+                for neighbor in Graph[ch]:
+                    indegree[neighbor] -= 1
+                    if indegree[neighbor] == 0:
+                        heappush(heap, neighbor)
+                        
+        # Order is invalid
+        if len(order) != len(indegree):
+            return ""
+        return ''.join(order)
+
+
+
+
+
+
+
+# 1876 · Alien Dictionary(easy)
+"""
+In an alien language, surprisingly they also use english lowercase letters, 
+but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+Given a sequence of words written in the alien language, and the order of the alphabet, 
+return true if and only if the given words are sorted lexicographically in this alien language.
+Otherwise, it returns false.
+"""
+class Solution:
+    """
+    @param words: the array of string means the list of words
+    @param order: a string indicate the order of letters
+    @return: return true or false
+    """
+    def is_alien_sorted(self, words, order: str) -> bool:
+        index = {value : index for index, value in enumerate(order)}
+        return words == sorted(words, key=lambda w: [index[x] for x in w])
